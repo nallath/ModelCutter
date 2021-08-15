246b052a37cab707cf48ece04cd43cc89a60958b
Jaime van Kessel <nallath@gmail.com>
2021-08-15 20:45:50 +0200

    Actually do the cutting

--- ChopperTool.py
+++ ChopperTool.py
@@ -1,3 +1,7 @@
+from UM.Math.AxisAlignedBox import AxisAlignedBox
+from UM.Operations.AddSceneNodeOperation import AddSceneNodeOperation
+from UM.Operations.GroupedOperation import GroupedOperation
+from UM.Operations.RemoveSceneNodeOperation import RemoveSceneNodeOperation
 from UM.Tool import Tool
 from UM.Event import Event
 from UM.Application import Application
@@ -6,14 +10,20 @@ from UM.Scene.SceneNode import SceneNode
 from UM.Mesh.MeshBuilder import MeshBuilder
 from UM.Event import Event, MouseEvent
 
+from UM.Mesh.MeshData import MeshData, calculateNormalsFromIndexedVertices
+
 from UM.Math.Plane import Plane
 from UM.Math.Vector import Vector
 from UM.Logger import Logger
-
+from cura.Scene.BuildPlateDecorator import BuildPlateDecorator
+from cura.Scene.CuraSceneNode import CuraSceneNode
 
 from .ChopperToolHandle import ChopperToolHandle
 from .PlaneNode import PlaneNode
+from .SetTransformMatrixOperation import SetTransformMatrixOperation
+from .SetParentOperationSimplified import SetParentOperationSimplified
 import numpy
+import trimesh
 
 from cura.Scene.SliceableObjectDecorator import SliceableObjectDecorator
 
@@ -49,8 +59,119 @@ class ChopperTool(Tool):
             self.propertyChanged.emit()
 
     def cutObject(self):
+        mesh_data = self._active_node.getMeshData().getTransformed(self._active_node.getWorldTransformation())
 
+
+        indices = numpy.arange(mesh_data.getVertexCount()).reshape(-1, 3)
+
+        trimesh_mesh = trimesh.base.Trimesh(vertices=mesh_data.getVertices(), faces=indices)
+
+
+        plane_position = self._plane_node.getPosition()
+
+        if self._cut_direction == "z":
+            plane_normal = [0, 1, 0]
+            inverse_plane_normal = [0, -1, 0]
+        elif self._cut_direction == "x":
+            plane_normal = [1, 0, 0]
+            inverse_plane_normal = [-1, 0, 0]
+        else:
+            plane_normal = [0, 0, 1]
+            inverse_plane_normal = [0, 0, -1]
+
+        half_one = trimesh.intersections.slice_mesh_plane(trimesh_mesh, plane_normal= plane_normal, plane_origin=[plane_position.x, plane_position.y, plane_position.z], cap=True)
+        half_two = trimesh.intersections.slice_mesh_plane(trimesh_mesh, plane_normal= inverse_plane_normal, plane_origin=[plane_position.x, plane_position.y, plane_position.z], cap=True)
+        #half_one.show(smooth=False)
+        #half_two.show(smooth=False)
+        self._replaceSceneNode(self._active_node, [half_one, half_two])
+        op = GroupedOperation()
+        op.addOperation(RemoveSceneNodeOperation(self._active_node))
         return
+
+    def _toMeshData(self, tri_node: trimesh.base.Trimesh, file_name: str = "") -> MeshData:
+        tri_faces = tri_node.faces
+        tri_vertices = tri_node.vertices
+
+        indices = []
+        vertices = []
+
+        index_count = 0
+        face_count = 0
+        for tri_face in tri_faces:
+            face = []
+            for tri_index in tri_face:
+                vertices.append(tri_vertices[tri_index])
+                face.append(index_count)
+                index_count += 1
+            indices.append(face)
+            face_count += 1
+
+        vertices = numpy.asarray(vertices, dtype=numpy.float32)
+        indices = numpy.asarray(indices, dtype=numpy.int32)
+        normals = calculateNormalsFromIndexedVertices(vertices, indices, face_count)
+
+        mesh_data = MeshData(file_name=file_name, vertices=vertices, indices=indices, normals=normals)
+        return mesh_data
+
+    def _replaceSceneNode(self, existing_node, trimeshes) -> None:
+        name = existing_node.getName()
+        file_name = existing_node.getMeshData().getFileName()
+        transformation = existing_node.getWorldTransformation()
+        parent = existing_node.getParent()
+        extruder_id = existing_node.callDecoration("getActiveExtruder")
+        build_plate = existing_node.callDecoration("getBuildPlateNumber")
+        selected = Selection.isSelected(existing_node)
+
+        children = existing_node.getChildren()
+        new_nodes = []
+
+        op = GroupedOperation()
+        op.addOperation(RemoveSceneNodeOperation(existing_node))
+
+        for i, tri_node in enumerate(trimeshes):
+            mesh_data = self._toMeshData(tri_node, file_name)
+
+            new_node = CuraSceneNode()
+            new_node.setSelectable(True)
+            new_node.setMeshData(mesh_data)
+            new_node.setName(name if i == 0 else "%s %d" % (name, i))
+            new_node.callDecoration("setActiveExtruder", extruder_id)
+            new_node.addDecorator(BuildPlateDecorator(build_plate))
+            new_node.addDecorator(SliceableObjectDecorator())
+
+            op.addOperation(AddSceneNodeOperation(new_node, parent))
+            op.addOperation(SetTransformMatrixOperation(new_node, transformation))
+
+            new_nodes.append(new_node)
+
+            if selected:
+                Selection.add(new_node)
+
+        for child in children:
+            mesh_data = child.getMeshData()
+            if not mesh_data:
+                continue
+            child_bounding_box = mesh_data.getTransformed(child.getWorldTransformation()).getExtents()
+            if not child_bounding_box:
+                continue
+            new_parent = None
+            for potential_parent in new_nodes:
+                parent_mesh_data = potential_parent.getMeshData()
+                if not parent_mesh_data:
+                    continue
+                parent_bounding_box = parent_mesh_data.getTransformed(
+                    potential_parent.getWorldTransformation()).getExtents()
+                if not parent_bounding_box:
+                    continue
+                intersection = child_bounding_box.intersectsBox(parent_bounding_box)
+                if intersection != AxisAlignedBox.IntersectionResult.NoIntersection:
+                    new_parent = potential_parent
+                    break
+            if not new_parent:
+                new_parent = new_nodes[0]
+            op.addOperation(SetParentOperationSimplified(child, new_parent))
+
+        op.push()
 
 
     def getIsCutting(self):
---
+++ SetTransformMatrixOperation.py
@@ -0,0 +1,46 @@
+# Copyright (c) 2018 Aldo Hoeben / fieldOfView.
+# This code was copied from MeshTools, which was released under the terms of the AGPLv3 or higher.
+
+from UM.Operations.SetTransformOperation import SetTransformOperation
+from UM.Operations.Operation import Operation
+from typing import Union
+
+##  Operation that translates, rotates and scales a node all at once.
+class SetTransformMatrixOperation(SetTransformOperation):
+    ##  Creates the transform operation.
+    #
+    #   \param node The scene node to transform.
+    #   \param transform A fully formed transformation matrix to transform the node with.
+    def __init__(self, node, transform = None) -> None:
+        super().__init__(node)
+
+        self._old_transformation = node.getWorldTransformation()
+        self._new_transformation = transform
+
+    ##  Merges this operation with another SetTransformMatrixOperation.
+    #
+    #   This prevents the user from having to undo multiple operations if they
+    #   were not his operations.
+    #
+    #   You should ONLY merge this operation with an older operation. It is NOT
+    #   symmetric.
+    #
+    #   \param other The older operation with which to merge this operation.
+    #   \return A combination of the two operations, or False if the merge
+    #   failed.
+    def mergeWith(self, other) -> Union[Operation, bool]:
+        if type(other) is not SetTransformMatrixOperation:
+            return False
+        if other._node != self._node: # Must be on the same node.
+            return False
+
+        op = SetTransformMatrixOperation(self._node)
+        op._old_transformation = other._old_transformation
+        op._new_transformation = self._new_transformation
+        return op
+
+    ##  Returns a programmer-readable representation of this operation.
+    #
+    #   A programmer-readable representation of this operation.
+    def __repr__(self) -> str:
+        return "SetTransformMatrixOperation(node = {0})".format(self._node)
